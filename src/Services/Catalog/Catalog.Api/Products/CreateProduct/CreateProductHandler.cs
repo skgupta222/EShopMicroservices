c3202c76c64571b2be188658d15a8fa66f34001b
3PILLAR\sunil.gupta <sunil.gupta@3pillarglobal.com>
2025-01-10 16:31:02 +0530

    Catelog.API

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,26 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Api.Models;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+      : ICommand<CreateProductResult> ;
+    public record CreateProductResult(Guid id);
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name= command.Name,
+                Category= command.Category,
+                Description= command.Description,
+                ImageFile= command.ImageFile,
+                Price= command.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+            
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Program.cs
@@ -0,0 +1,17 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeling
+
+app.MapCarter();
+
+app.Run();
